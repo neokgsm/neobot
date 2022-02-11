399318974dca2f4a84575b62a1fa106bb67799af
neokgsm <79746975+neokgsm@users.noreply.github.com>
2022-02-11 17:46:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -63,27 +63,8 @@ async def _bot(ctx):
     """Is the bot cool?"""
     await ctx.send('Yes, the bot is cool.')
 
-@bot.command(name="8ball")
-async def _8ball(ctx):
-    ans = ["It is certain.",
-           "It is decidedly so.",
-           "Without a doubt.",
-           "Yes definitely.",
-           "You may rely on it.",
-           "As I see it, yes.",
-           "Most likely.",
-           "Outlook good.",
-           "Yes.;Signs point to yes.",
-           "Reply hazy try again.",
-           "Ask again later.",
-           "Better not tell you now.",
-           "Cannot predict now.",
-           "Concentrate and ask again.",
-           "Don't count on it.",
-           "My reply is no.",
-           "My sources say no.",
-           "Outlook not so good.",
-           "Very doubtful."]
-    await ctx.send(f'@{ctx.subcommand_passed}, {ans[random.randrange(len(ans))]})
-
+@test.command()
+async def test(ctx):
+    await ctx.send(ctx.subcommand_passed)
+    
 bot.run(os.environ['DISCORD_BOT_TOKEN'])
