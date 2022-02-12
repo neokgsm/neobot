df3313b9c349f0237368085ee7f8ff2e1507434f
neokgsm <79746975+neokgsm@users.noreply.github.com>
2022-02-12 16:00:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -164,7 +164,7 @@ async def color(ctx, nitro_color=""):
 @bot.command()
 async def colors(ctx):
     server = ctx.author.guild
-    color_names = [nc.name for nc in server.roles if 'nitro' in nc.name]
+    color_names = [nc.name for nc in server.roles if 'nitro' in nc.name].reverse()
     txt = str(color_names).replace("'", "").replace(",", "")
     await ctx.send(f'These are the available colors: {txt}')
 
